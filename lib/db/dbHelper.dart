86bb3f44cfb5ddd1dd4d9558670bde017f9aee43
Shoykat361 <shoykat15-11724@diu.edu.bd>
2024-05-01 00:47:41 +0600

    db helper class added

--- lib/db/dbHelper.dart
+++ lib/db/dbHelper.dart
@@ -1,4 +1,34 @@
 import 'package:path/path.dart'as p;
+import 'package:sqflite/sqflite.dart';
+import 'package:virtual_card/models/contact_model.dart';
 class dbHelper{
+  final String _createContact = '''
+  create table $tableContact(
+  $tableContactId integer  primary key autoincrement,
+  $tableContactName text,
+  $tableContactMobile text,
+  $tableContactEmail text,
+  $tableContactAddress text,
+  $tableContactDesignation text,
+  $tableContactWebsite text,
+  $tableContactImage text,
+  $tableContactFavourite text)''';
+  Future<Database> _open() async{
+    final root = await getDatabasesPath();
+    final dbpath = p.join(root,'contact,db');
+    return openDatabase(dbpath,version: 1,onCreate: (db,version){
+      db.execute(_createContact);
+    });
+
+  }
+  Future<int> insertContact(ContactModel contactModel) async{
+    final db = await _open();
+    return db.insert(tableContact, contactModel.toMap());
+  }
+  Future<List<ContactModel>> getAllConact() async {
+    final db = await _open();
+    final mapList = await db.query(tableContact);
+    return List.generate(mapList.length, (index) => ContactModel.fromMap(mapList[index]));
+  }
 
 }
